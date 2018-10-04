c2f41acd384f855a1ef23d593b3602c17b0d4ba7
Sondrbe <sondrebergo@gmail.com>
2018-10-04 10:19:02 +0200

    Better plotting

--- sharpeRatio.py
+++ sharpeRatio.py
@@ -9,10 +9,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import simps
 
-Es = stdevs = [0.03, 0.08, 0.15, 0.30, 0.40]
+noRisk = 0.0077
+Es = [0.03, 0.08, 0.15, 0.30, 0.40]
+stdevs = [x-noRisk for x in Es]
 
 def sharpeRatio(E, stdev):
-    return E, stdev
+    return (E-noRisk) / stdev
 
 def normal(x, E, stdev):
     return 1/(np.sqrt(2*np.pi*stdev**2))*np.exp(-(x-E)**2/(2*stdev**2))
@@ -20,6 +22,11 @@ def normal(x, E, stdev):
 normal_x = np.linspace(E-3,E+5,200)
 normalDist = normal(normal_x, 1+E, 1+stdev)
 #plt.plot(normal_x, normalDist)
+
+for E, stdev in zip(Es, stdevs):
+    normal_x = np.linspace(E-3,E+5,200)
+    normalDist = normal(normal_x, 1+E, 1+stdev)
+    plt.plot(normal_x, normalDist)    
 
 
 x = np.linspace(0,365,365)
@@ -43,11 +50,11 @@ for E,stdev,col in zip(Es, stdevs,['r','y','b','g','m']):
             break
     E_std_min = normal_x[ind2]*E
 
-    for E_spread in np.linspace(E_std_min, E_std_max):
+    for E_spread in np.linspace(E_std_min, E_std_max, 120):
         E_cum = np.cumprod(np.array([E_spread/365+1.]*365))
-        plt.plot(x,E_cum, col)
-    plt.plot(x,E_cum, col, alpha=0.4)        
-    E_mean = np.cumprod(np.array([E/365+1.]*365))
-    plt.plot(x, E_mean, col, linewidth=3., label='E'+str(counter))
+        plt.plot(x,E_cum, col, alpha=0.4)
+    #plt.plot(x,E_cum, col, alpha=0.4)        
+    #E_mean = np.cumprod(np.array([E/365+1.]*365))
+    #plt.plot(x, E_mean, col, alpha=0.4, linewidth=3., label='E'+str(counter))
 plt.legend(loc='best')
 
